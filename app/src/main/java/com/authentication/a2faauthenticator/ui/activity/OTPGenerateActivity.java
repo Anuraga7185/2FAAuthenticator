46ad87dd7c918830be115f3f1c53130fea74643a
Anuraga7185 <ranurag378@gmail.com>
2025-06-20 19:46:20 +0530

    Code for CICD

--- app/src/main/java/com/authentication/a2faauthenticator/ui/activity/OTPGenerateActivity.java
+++ app/src/main/java/com/authentication/a2faauthenticator/ui/activity/OTPGenerateActivity.java
@@ -84,7 +84,6 @@ public class OTPGenerateActivity extends AppCompatActivity {
                 Log.d("OTP GENERATE ACTIVITY", "EMPTY SECRET KEY    ");
                 return;
             }
-
             viewModel.insertAuthDetail(authDetail);
         } else {
             Log.d("OTP GENERATE ACTIVITY", "NO DATA");
